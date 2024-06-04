9deb3911cf2a6ed5302b89fb9ff26bdf8faed4c2
PasinduBm <pasindumunasinghe94@gmail.com>
2024-06-04 18:54:58 +0530

    corrections on redesign

--- src/app/redux/index.js
+++
@@ -1,7 +0,0 @@
-// store/index.js
-import { createWrapper } from 'next-redux-wrapper';
-import { store } from './store';
-
-const makeStore = () => store;
-
-export const wrapper = createWrapper(makeStore);
--- src/app/redux/store.js
+++
@@ -1,9 +0,0 @@
-// src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import emotionReducer from './emotionSlice';
-
-export const store = configureStore({
-  reducer: {
-    emotions: emotionReducer,
-  },
-});
